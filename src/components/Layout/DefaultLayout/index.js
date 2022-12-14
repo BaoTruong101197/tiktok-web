fdd3fce68c9f8ba11085cc12ca5db02d8d07d65d
“Your <you@example.com>
2022-12-15 00:48:47 +0700

    Build base default layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind'
 import Header from '~/components/Layout/components/Header'
 import Sidebar from './Sidebar'
+import styles from './DefaultLayout.module.scss'
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
     return (
-        <h1 className="container">
+        <h1 className={cx('wrapper')}>
             <Header />
-            <div className="content">
+            <div className={cx('content')}>
                 <Sidebar />
-                {children}
+                <div className={cx('container')}>{children}</div>
             </div>
         </h1>
     )
