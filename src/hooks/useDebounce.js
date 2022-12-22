33357dccecdafa8cb15afae023ac272ca2709227
“Your <you@example.com>
2022-12-22 23:42:10 +0700

    fix bug debounce

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from 'react'
 function useDebounce(value, delay) {
     const [debounceValue, setDebounceValue] = useState(value)
 
+    if (value === '') {
+        delay = 0
+    }
+
     useEffect(() => {
         const handler = setTimeout(() => {
             setDebounceValue(value)
