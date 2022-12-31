ead628f61e83ee225e8d54dd0cdaa2e15545d815
“Your <you@example.com>
2022-12-31 23:16:48 +0700

    add animation lib and implement animation for menu

--- src/components/Popper/Menu/Menu.js
+++ src/components/Popper/Menu/Menu.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSpring, motion } from 'framer-motion'
 import PropTypes from 'prop-types'
 import Tippy from '@tippyjs/react/headless'
 import classNames from 'classnames/bind'
@@ -16,6 +17,10 @@ function Menu({ children, items = [] }) {
     const [, dispatch] = useContextProvider()
     const [history, setHistory] = useState([{ data: items }])
     const currentTab = history[history.length - 1]
+
+    // Animation
+    const springConfig = { damping: 15, stiffness: 150 }
+    const opacity = useSpring(0, springConfig)
 
     const renderItem = () => {
         return currentTab.data.map((item, index) => {
@@ -42,21 +47,32 @@ function Menu({ children, items = [] }) {
         setHistory(history.slice(0, history.length - 1))
     }
 
+    function onMount() {
+        opacity.set(1)
+    }
+
+    function onHide({ unmount }) {
+        opacity.set(0)
+        setHistory(history.slice(0, 1))
+    }
+
     return (
         <Tippy
             interactive
             offset={[13, 8]}
             placement="bottom-end"
-            onHide={() => setHistory(history.slice(0, 1))}
+            animation={true}
+            onMount={onMount}
+            onHide={onHide}
             delay={[0, 800]}
             hideOnClick={false}
             render={attrs => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                <motion.div className={cx('menu-list')} tabIndex="-1" style={{ opacity }} {...attrs}>
                     <PopperWrapper className={cx('menu-wrapper')}>
                         {history.length > 1 && <Header onBack={onBack} title={currentTab.title} />}
                         <div className={cx('menu-content')}>{renderItem()}</div>
                     </PopperWrapper>
-                </div>
+                </motion.div>
             )}
         >
             {children}
